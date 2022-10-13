705f2090396b5c4109ead0c8959c20cb40dd7c1c
Víctor Barreto <victorbarretolins@gmail.com>
2022-10-13 10:58:54 -0300

    adding items to cart Pt.1

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,18 @@
-import Button from '../button/button.component'
+import Button from "../button/button.component";
+
+import CartItem from "../cart-item/cart-item.component";
 
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
-  return <div className="cart-dropdown-container">
-    <div className="cart-items">
-    <Button>GO TO CHECKOUT</Button>
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        
+      </div>
+      <Button>CHECKOUT</Button>
     </div>
-  </div>;
+  );
 };
 
 export default CartDropdown;
